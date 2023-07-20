40f4d4744f31f9dd20cb87a1dc0d7be3e710ce25
finebizzle <finebizzle@users.noreply.github.com>
2023-07-20 14:45:49 +0100

    Update image-grid.js

--- image-grid.js
+++ image-grid.js
@@ -1,53 +1,70 @@
-function renderImageGrid(data, container) {
+function renderImageGrid(data, container, config) {
   // Define the dimensions and properties of the image grid
   const imageWidth = 90;
   const imageHeight = 90;
   const imagesPerRow = 20;
   const spacing = 10;
+  const maxImages = config.maxImages || 500; // Maximum number of images to display
 
-  // Calculate the total number of rows based on the data length and images per row
-  const numRows = Math.ceil(data.length / imagesPerRow);
+  // Take the first `maxImages` elements from the data array that have images
+  const truncatedData = data.filter(d => d[Object.keys(d)[2]] && d[Object.keys(d)[2]].value).slice(0, maxImages);
+
+  // Calculate the total number of rows based on the truncated data length and images per row
+  const numRows = Math.ceil(truncatedData.length / imagesPerRow);
+
+  // Calculate the number of images to display in the last row
+  const imagesInLastRow = truncatedData.length % imagesPerRow || imagesPerRow;
 
   // Create a container for the image grid
   const gridContainer = container.append('div')
     .style('display', 'flex')
     .style('flex-wrap', 'wrap');
 
-  // Add image elements to the grid container
-  const images = gridContainer.selectAll('.image')
-    .data(data)
-    .enter()
-    .append('div')
-    .style('width', `${imageWidth}px`)
-    .style('height', `${imageHeight}px`)
-    .style('margin', `${spacing}px`)
-    .style('background-color', '#eee')
-    .style('background-size', 'cover')
-    .style('background-position', 'center')
-    .style('background-image', d => `url(${d[Object.keys(d)[2]].value})`);
+  // Loop through the truncated data and add image elements to the grid container
+  truncatedData.forEach((d, index) => {
+    const imageURL = d[Object.keys(d)[0]].value;
 
-  // Add image names as labels at the bottom of each cell
-  images.append('div')
-    .style('bottom', '0')
-    .style('left', '0')
-    .style('right', '0')
-    .style('padding', '5px')
-    .style('background-color', 'rgba(0, 0, 0, 0.7)')
-    .style('color', '#fff')
-    .style('text-align', 'center')
-    .text(d => '$' + d[Object.keys(d)[3]].value.toFixed(2));
+    const imageContainer = gridContainer.append('div')
+      .style('width', `${imageWidth}px`)
+      .style('height', `${imageHeight}px`)
+      .style('margin', `${spacing}px`)
+      .style('background-color', '#eee')
+      .style('background-size', 'cover')
+      .style('background-position', 'center')
+      .style('background-image', `url(${imageURL})`);
+
+  
+  });
+
+  // Add empty elements to fill the last row if needed
+  if (truncatedData.length < imagesPerRow) {
+    for (let i = 0; i < imagesPerRow - truncatedData.length; i++) {
+      gridContainer.append('div')
+        .style('width', `${imageWidth}px`)
+        .style('height', `${imageHeight}px`)
+        .style('margin', `${spacing}px`)
+        .style('background-color', 'transparent');
+    }
+  }
 
   // Adjust the grid container height based on the number of rows
   const gridHeight = numRows * (imageHeight + 2 * spacing);
   gridContainer.style('height', `${gridHeight}px`);
 }
 
-
-
 const vis = {
   id: 'image-grid',
   label: 'Image Grid',
-  options: {},
+  options: {
+    maxImages: {
+      label: 'Max Images',
+      default: 500,
+      type: 'number',
+      display: 'text',
+      section: 'Data',
+      placeholder: 'Enter the maximum number of images to display',
+    },
+  },
   create(element, config) {
     element.innerHTML = '<div class="image-grid"></div>';
     return {};
@@ -56,7 +73,7 @@ const vis = {
     // Render the image grid
     const container = d3.select(element).select('.image-grid');
     const errorMessageElement = element.querySelector('.error-message');
-    renderImageGrid(data, container);
+    renderImageGrid(data, container, config);
 
     try {
       // Clear any previous error messages
@@ -64,7 +81,6 @@ const vis = {
 
       // Clear the table content
       tableContainer.innerHTML = '';
-      
 
       // Render the image table
       renderImageTable(data, d3.select(tableContainer), config, queryResponse);
@@ -76,3 +92,4 @@ const vis = {
 };
 
 looker.plugins.visualizations.add(vis);
+
