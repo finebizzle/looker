2c5830f8d2b5dd8f558a4c18874439890c3b35c7
finebizzle <finebizzle@users.noreply.github.com>
2023-09-11 10:47:50 +0100

    Update image_table_original.js

--- image_table_original.js
+++ image_table_original.js
@@ -1,49 +1,127 @@
 function renderImageTable(data, container, config) {
-  // ... (previous code)
+  // Create a container for the image table
+  const tableContainer = container.append('table')
+    .style('border-collapse', 'collapse');
 
-  // Add the image cells for the current statement month
+  // Extract the unique statement months from the data objects
+  // const statementMonths = Array.from(new Set(data.map(d => d['content_partner.statement_month'].value)));
+
+    const statementMonths = Array.from(new Set(data.map(d => d[Object.keys(d)[1]].value)));
+
+  // Extract the unique rank_product_id values
+  // const uniqueRankProductIds = Array.from(new Set(data.map(d => d['content_partner.rank_product_id'].value)));
+
+  const uniqueRankProductIds = Array.from(new Set(data.map(d => d[Object.keys(d)[6]].value)));
+
+  // Create the table header row
+  const headerRow = tableContainer.append('tr');
+
+  // Get the custom labels from the options
+  const statementMonthLabel = config.statementMonthLabel || 'Dimension';
+  const measureLabel1 = config.MeasureLabel || 'Measure';
+  const measureLabel2 = config.MeasureLabel2 || '';
+
+  // Add the statement month header
+  headerRow.append('th')
+    .text(statementMonthLabel)
+    .style('background-color', config.headerColor)
+    .style('color', config.headerFontColor)
+    .style('font-size', config.fontSize)
+    .style('font-family', config.fontFamily)
+    .style('padding', config.headerPadding);
+
+  // Add the image headers
   uniqueRankProductIds.forEach(rankProductId => {
-    const imageData = monthData.find(d => d[Object.keys(d)[5]].value === rankProductId);
-    const imageUrl = imageData ? imageData[Object.keys(imageData)[2]].value  : ''; // Use the retrieved dimension name
-    const mediaType = imageData ? imageData[config.queryResponse.fields.measure_like[0].name] : ''; // Use the retrieved measure name
-    const revenue = imageData ? formatNumericValue(imageData[Object.keys(imageData)[4]].value, config.numericFormat) : '';
-    const id = imageData ? imageData[config.queryResponse.fields.dimension_like[0].name] : '';
-
-    assetRow.append('td')
-      .style('background-color', config.rowColor2)
-      .style('color', config.fontColor)
+    headerRow.append('th')
+      .style('background-color', config.headerColor)
+      .style('color', config.headerFontColor)
       .style('font-size', config.fontSize)
       .style('font-family', config.fontFamily)
       .style('padding', config.headerPadding)
-      .text(id);
+      .text(rankProductId);
+  });
 
-    if (imageUrl) {
+  // Iterate over each statement month
+  statementMonths.forEach(month => {
+    // Filter the data for the current statement month
+    // const monthData = data.filter(d => d['content_partner.statement_month'].value === month);
+    const monthData = data.filter(d => d[Object.keys(d)[1]].value === month);
+    // Create a table row for the current statement month
+    const assetRow = tableContainer.append('tr');
+    const row = tableContainer.append('tr')
+      .style('border-top', '1px solid #ccc');
+    const revenueRow = tableContainer.append('tr');
+    // Add the statement month cell
+    assetRow.append('td')
+      .text(measureLabel2)
+      .style('background-color', config.rowColor2)
+      .style('color', config.headerFontColor)
+      .style('font-size', config.fontSize)
+      .style('font-family', config.fontFamily)
+      .style('padding', config.headerPadding)
+      .style('border-right', '1px solid #ccc');
+    
+    row.append('td')
+      .text(month)
+      .style('background-color', config.headerColor)
+      .style('color', config.headerFontColor)
+      .style('font-size', config.fontSize)
+      .style('font-family', config.fontFamily)
+      .style('padding', config.headerPadding)
+      .style('border-right', '1px solid #ccc');
+    revenueRow.append('td')
+      .text(measureLabel1)
+      .style('background-color', config.rowColor1)
+      .style('color', config.headerFontColor)
+      .style('font-size', config.fontSize)
+      .style('font-family', config.fontFamily)
+      .style('padding', config.headerPadding)
+      .style('border-right', '1px solid #ccc');
+    
+
+    // Add the image cells for the current statement month
+    uniqueRankProductIds.forEach(rankProductId => {
+      const imageData = monthData.find(d => d[Object.keys(d)[5]].value === rankProductId);
+      const imageUrl = imageData ? imageData[Object.keys(imageData)[2]].value  : '';
+      const mediaType = imageData ? imageData[Object.keys(imageData)[3]].value : '';
+      const revenue = imageData ? formatNumericValue(imageData[Object.keys(imageData)[5]].value, config.numericFormat) : '';
+      const id = imageData ? imageData[Object.keys(imageData)[0]].value : '';
+      assetRow.append('td')
+        .style('background-color', config.rowColor2)
+        .style('color', config.fontColor)
+        .style('font-size', config.fontSize)
+        .style('font-family', config.fontFamily)
+        .style('padding', config.headerPadding)
+        .text(id);
+
       const mediaCell = row.append('td').style('padding', '5px');
       if (mediaType !== 'VIDEO') {
         mediaCell.append('img')
           .style('width', config.imageWidth)
           .style('height', '90px')
-          .attr('src', imageUrl)
-          .attr('alt', 'Pulled');
+          .attr('src', imageUrl);
       } else if (mediaType === 'VIDEO') {
         mediaCell.append('video')
           .style('width', '100px')
           .style('height', config.imageWidth)
-          .attr('src', imageUrl)
-          .attr('alt', 'Pulled');
+          .attr('src', imageUrl);
       }
-    }
 
-    revenueRow.append('td')
-      .style('background-color', config.rowColor1)
-      .style('color', config.fontColor)
-      .style('font-size', config.fontSize)
-      .style('font-family', config.fontFamily)
-      .style('padding', config.headerPadding)
-      .text(revenue);
+      revenueRow.append('td')
+        .style('background-color', config.rowColor1)
+        .style('color', config.fontColor)
+        .style('font-size', config.fontSize)
+        .style('font-family', config.fontFamily)
+        .style('padding', config.headerPadding)
+        .text(revenue);
+
+     
+    });
   });
 
-  // ... (remaining code)
+  // Adjust the table cell borders
+  tableContainer.selectAll('td, th')
+    .style('border', '1px solid #ccc');
 }
 
 function formatNumericValue(value, format) {
@@ -58,8 +136,89 @@ function formatNumericValue(value, format) {
 }
 
 const vis = {
-  // ... (rest of your code)
-
+  id: 'image-table',
+  label: 'Image Table',
+  options: {
+    fontFamily: {
+      type: 'string',
+      label: 'Font Family',
+      default: 'Arial',
+    },
+    fontSize: {
+      type: 'string',
+      label: 'Font Size',
+      default: '12px',
+    },
+    fontColor: {
+      type: 'string',
+      label: 'Font Color',
+      default: '#000',
+    },
+    headerColor: {
+      type: 'string',
+      label: 'Header Color',
+      default: '#ccc',
+    },
+    headerFontColor: {
+      type: 'string',
+      label: 'Header Font Color',
+      default: '#000',
+    },
+    headerPadding: {
+      type: 'string',
+      label: 'Header Padding',
+      default: '5px',
+    },
+    rowColor1: {
+      type: 'string',
+      label: 'Row Color 1',
+      default: '#fff',
+    },
+    rowColor2: {
+      type: 'string',
+      label: 'Row Color 2',
+      default: '#fff',
+    },
+    imageWidth: {
+      type: 'string',
+      label: 'Image Width',
+      default: '90px',
+    },
+    imageHeight: {
+      type: 'string',
+      label: 'Image Height',
+      default: '90px',
+    },
+    cellPadding: {
+      type: 'string',
+      label: 'Cell Padding',
+      default: '5px',
+    },
+    statementMonthLabel: {
+      type: 'string',
+      label: 'Dimension',
+      default: '',
+    },
+    MeasureLabel: {
+      type: 'string',
+      label: 'Measure 1',
+      default: '',
+    },
+    MeasureLabel2: {
+      type: 'string',
+      label: 'Measure 2',
+      default: '',
+    },
+    numericFormat: {
+      type: 'string',
+      label: 'Numeric Format',
+      default: '',
+    },
+  },
+  create(element, config) {
+    element.innerHTML = '<div class="image-table"></div><div class="error-message" style="color: red; font-weight: bold;"></div>';
+    return {};
+  },
   update(data, element, config, context) {
     const tableContainer = element.querySelector('.image-table');
     const errorMessageElement = element.querySelector('.error-message');
@@ -71,23 +230,11 @@ const vis = {
       // Clear the table content
       tableContainer.innerHTML = '';
 
-      // Extract the dimension and measure names from the queryResponse
-      const dimensionName = config.queryResponse.fields.dimension_like[0].name;
-      const measureName = config.queryResponse.fields.measure_like[0].name;
+      // Get the revenue measure object based on position (index)
+      const revenueMeasure = config.query_fields.measures[0].label_short;
 
-      // Update the code to use the retrieved dimension and measure names
-      // Add the image cells for the current statement month
-      uniqueRankProductIds.forEach(rankProductId => {
-        const imageData = monthData.find(d => d[Object.keys(d)[5]].value === rankProductId);
-        const imageUrl = imageData ? imageData[Object.keys(imageData)[2]].value : ''; // Use the retrieved dimension name
-        const mediaType = imageData ? imageData[measureName] : ''; // Use the retrieved measure name
-        const revenue = imageData ? formatNumericValue(imageData[Object.keys(imageData)[4]].value, config.numericFormat) : '';
-        const id = imageData ? imageData[dimensionName] : '';
-
-        // Rest of the code remains the same
-        // ...
-
-      });
+      // Update the config with the revenue label as MeasureLabel
+      config.MeasureLabel = revenueMeasure ;
 
       // Call the renderImageTable function with the correct arguments
       renderImageTable(data, d3.select(tableContainer), config);
